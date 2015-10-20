29dab996b06ca599a6d6f5d82da15ca86f63e7b5
Xiaomin Wu <xiaowu@microsoft.com>
2015-10-20 09:56:14 -0700

    Update to use latest Gin.

--- gotry.go
+++ gotry.go
@@ -89,9 +89,13 @@ func main() {
 
     g := gin.Default()
     g.Use(GinLogger(f))
-    g.LoadHTMLGlob("D:\\home\\site\\wwwroot\\GinTemplate\\*")
+    if os.Getenv("HTTP_PLATFORM_PORT") != "" {
+      g.LoadHTMLFiles("D:\\home\\site\\wwwroot\\index-gin.html")
+    } else {
+      g.LoadHTMLFiles("index-gin.html")
+    }
     g.GET("/g/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.tmpl", gin.H{
+        c.HTML(http.StatusOK, "index-gin.html", gin.H{
             "title" : "Hello from Gin!",
         })
     })
